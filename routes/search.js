f3b04a67ccec7687f4a734059c59403126ff9298
Francesco Nero <nerofrancesco@hotmail.com>
2015-01-16 10:31:17 +0100

    Merge branch 'master' of /home/francesco/Progetti/RottenBooks with conflicts.

--- routes/search.js
+++ routes/search.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 var querystring = require('querystring');
-
-
+var cheerio = require('cheerio');
+var books = require('google-books-search')
 
 //VARIABILI PER PRODURRE PATH DI RICHIESTA
 var partialQueryMovie = '/api/public/v1.0/movies.json?apikey=';
 var apiKeyMovie = 'xxcrmh8fb44ab9qukqr9426d';
 var hostMovie = 'api.rottentomatoes.com';
 var queryTermMovie="";
+
+//VARIABILI PER RICHIESTA IMDB
+var hostIMDB = 'www.imdb.com'
+var pathIMDB = '/title/tt_ID/literature'
 
 function film(data) {
     data.posters.detailed = data.posters.detailed.replace('_tmb', '_det')
@@ -20,6 +24,14 @@ function film(data) {
     this.criticsScore = data.ratings.critics_score;
     this.audienceScore = data.ratings.audience_score;
     this.posters = data.posters;
+    alternate_ids = data.alternate_ids || {};
+    this.imdb = alternate_ids.imdb
+}
+
+function Book(data) {
+    this.title = data.title
+    this.link = data.link
+    this.thumbnail = data.thumbnail
 }
 
 function httpGetMovie(response)
@@ -31,6 +43,7 @@ function httpGetMovie(response)
     {
         'Content-Type': 'application/json'
     };
+
     var optionsMovie =
     {
         host: hostMovie,
@@ -57,6 +70,52 @@ function httpGetMovie(response)
 
                 for (var i = 0; i < content.movies.length; i++) {
                     films[i] = new film(content.movies[i]);
+                    console.log(films[i])
+
+                    //GO LOOK FOR THE BOOK
+                    if(films[i].imdb) {
+                        var optionsIMDB =
+                        {
+                            host: hostIMDB,
+                            path: pathIMDB.replace('_ID', films[i].imdb),
+                            method: 'GET'
+                        };
+                        console.log(optionsIMDB.host+optionsIMDB.path)
+
+                        var reqIMDB = http.request(optionsIMDB,
+                            function (resIMDB) {
+                                var IMDBStringResponse = '';
+
+                                resIMDB.on('data', function (piece) {
+                                    IMDBStringResponse += piece;
+                                });
+
+                                resIMDB.on('end', function () {
+                                    var contentIMDB = cheerio.load(IMDBStringResponse);
+                                    var originalBook = contentIMDB('*').filter(function(i, el) {
+                                        return contentIMDB(this).text() === 'Original Literary Source';
+                                    })[0]
+                                    if(originalBook){
+                                        var author = originalBook.next['data'].match(/([^"]+)"/)[1]
+                                        var title = originalBook.next['data'].match(/"([^"]+)"/)[1]
+                                        console.log(author)
+                                        console.log(title)
+                                        books.search(title, function(error, results) {
+                                            if ( ! error ) {
+                                                console.log(results);
+                                            } else {
+                                                console.log(error);
+                                            }
+                                        });
+                                    }
+                                });
+                            });
+                        reqIMDB.on('error', function(e) {
+                            console.error("REQUEST TO IMDB FAILED")
+                            console.error(e);
+                        });
+                        reqIMDB.end();
+                    }
                 }
                 
                 response.render('index',{titolo: 'Welcome To RottenBooks' ,
@@ -64,11 +123,13 @@ function httpGetMovie(response)
                                          films: films});
             });
 
-            req.on('error', function(e) {
-                console.error(e);
-            });
 
         });
+
+    req.on('error', function(e) {
+        console.error(e);
+    });
+
     req.end();
 
 
